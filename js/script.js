8f31755f40acbda743fab59640d1444be585c9f5
Adam Ayd <adamayd@gmail.com>
2017-10-25 01:25:54 -0400

    fixed nav opacity on scroll

--- js/script.js
+++ js/script.js
@@ -9,6 +9,21 @@ function lightNav(ev) {
     navLinks.forEach(navLink => navLink.style.color = '#ffffff');
 }
 
+window.onscroll = function() {
+    // console.log(document.body.scrollTop);
+    if (document.body.scrollTop > (headerHeight - 40)) {
+        console.log('change the nav');
+        document.querySelector('nav').style.backgroundColor = 'rgba(0,0,0,1)';
+    } else {
+        console.log('leave the nav');
+        document.querySelector('nav').style.backgroundColor = 'rgba(0,0,0,0.2)';
+    }
+}
+
+// let scrollOffset = document.querySelector('header').offsetTop;
+let headerHeight = document.querySelector('header').scrollHeight;
+// console.log(scrollOffset, headerHeight);
+
 const navs = document.querySelectorAll('.nav-item');
 const navLinks = document.querySelectorAll('nav a');
 navs.forEach(nav => nav.addEventListener('mouseover', darkNav));
