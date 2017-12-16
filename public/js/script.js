eda8a951ed1c1e3c2d166cc94b8da21ad6446a30
Adam Ayd <adamayd@gmail.com>
2017-12-15 22:02:45 -0500

    portfolio slides complete with transition issue fix to come later

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,25 @@
-const navBar = document.querySelector('nav');
+const navBar = document.querySelector('nav')
+const portSlides = document.querySelectorAll('.port-slide')
 
 function navBG() {
-    // console.log(window.scrollY);
+    // console.log(document.scrollTop);
     if (window.scrollY > 100) {
-        navBar.classList.add('dark-nav');
+        navBar.classList.add('dark-nav')
     } else {
-        navBar.classList.remove('dark-nav');
+        navBar.classList.remove('dark-nav')
     }
+}
+
+function portTitleAppear () {
+    // console.log(this)
+    const portTitle = this.querySelector('.port-title')
+    const portButton = this.querySelector('.port-button')
+    this.classList.toggle('port-slide-active')
+    portTitle.classList.toggle('port-title-active')
+    portButton.classList.toggle('port-button-active')
 }
 
 
 window.addEventListener('scroll', navBG);
+portSlides.forEach(portSlide => portSlide.addEventListener('mouseenter', portTitleAppear))
+portSlides.forEach(portSlide => portSlide.addEventListener('mouseleave', portTitleAppear))
