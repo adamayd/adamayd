1978447d975992fb81fa4cc0902be3b751600f81
Adam Ayd <adamayd@gmail.com>
2017-12-16 17:16:44 -0500

    cleaned up some divitis in blog section

--- public/js/script.js
+++ public/js/script.js
@@ -1,22 +1,27 @@
 const navBar = document.querySelector('nav')
 const portSlides = document.querySelectorAll('.port-slide')
+const navLinks = document.querySelectorAll('.nav-link')
 
 function navBG() {
     // console.log(document.scrollTop);
-    if (window.scrollY > 100) {
-        navBar.classList.add('dark-nav')
+    if (window.scrollY > 330) {
+        navBar.classList.add('nav--dark')
     } else {
-        navBar.classList.remove('dark-nav')
+        navBar.classList.remove('nav--dark')
     }
 }
 
-function portTitleAppear () {
+function portTitleAppear() {
     // console.log(this)
     const portTitle = this.querySelector('.port-title')
     const portButton = this.querySelector('.port-button')
     this.classList.toggle('port-slide-active')
     portTitle.classList.toggle('port-title-active')
     portButton.classList.toggle('port-button-active')
+}
+
+function addLifeGrid() {
+    
 }
 
 
