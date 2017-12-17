2c98e13033177d33238600463e7d372410a5fbfb
Adam Ayd <adamayd@gmail.com>
2017-12-17 02:47:17 -0500

    fixed nav issues and added nav item fade on hover

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,23 @@
 const navBar = document.querySelector('nav')
 const portSlides = document.querySelectorAll('.port-slide')
+const navList = document.querySelector('.nav-list')
 const navLinks = document.querySelectorAll('.nav-link')
 
 function navBG() {
     // console.log(document.scrollTop);
-    if (window.scrollY > 330) {
+    const winHeight = (window.innerHeight / 2) - (window.innerHeight / 7)
+    if (window.scrollY > winHeight) {
         navBar.classList.add('nav--dark')
     } else {
         navBar.classList.remove('nav--dark')
     }
 }
 
+function highlightNavLink() {
+    navLinks.forEach(navLink => navLink.classList.toggle('nav-link-dark'))
+}
+
 function portTitleAppear() {
-    // console.log(this)
     const portTitle = this.querySelector('.port-title')
     const portButton = this.querySelector('.port-button')
     this.classList.toggle('port-slide-active')
@@ -21,10 +26,13 @@ function portTitleAppear() {
 }
 
 function addLifeGrid() {
-    
+    const gridSize = window.innerWidth > 900 ? 10 : window.innerWidth > 768 ? 6 : 3
 }
 
 
 window.addEventListener('scroll', navBG);
+window.addEventListener('resize', addLifeGrid)
 portSlides.forEach(portSlide => portSlide.addEventListener('mouseenter', portTitleAppear))
 portSlides.forEach(portSlide => portSlide.addEventListener('mouseleave', portTitleAppear))
+navList.addEventListener('mouseenter', highlightNavLink)
+navList.addEventListener('mouseleave', highlightNavLink)
